bec86b14a003312138f619d6ba3678caf1151ba8
Sajmul <sajmul1427@gmail.com>
2025-07-08 22:00:28 +0600

    user routes created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import { UserRoutes } from "./app/modules/user/user.route";
+import cors from 'cors';
+
 const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/v1/user", UserRoutes);
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to tour management system",
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.create({
+      name,
+      email,
+    });
+
+    res.status(httpStatus.ACCEPTED).json({
+      message: "User created Successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "Something Went Wrong",
+      error,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
