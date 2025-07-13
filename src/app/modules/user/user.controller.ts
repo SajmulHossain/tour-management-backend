9486c8c109d3428c776ec8e90372529a268bfdf2
Sajmul <sajmul1427@gmail.com>
2025-07-14 00:06:37 +0600

    type extends for Request, added user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -42,8 +42,7 @@ const createUser = catchAsync(
 const updateUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.params.id;
-    const token = req.headers.authorization;
-    const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload;
+    const verifiedToken = req.user;
     const user = await UserServices.updateUser(userId,req.body,verifiedToken);
 
     sendResponse(res, {
