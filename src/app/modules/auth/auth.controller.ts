41e0070d2e84e08780171850cd29b4bd89ab629f
Sajmul <sajmul1427@gmail.com>
2025-07-14 01:19:18 +0600

    access token and refresh token set to frontend is done

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,37 +1,49 @@
 import { Request, Response } from "express";
-import httpStatus from 'http-status-codes';
-import { catchAsync } from '../../utils/catchAsync';
+import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import { AuthServices } from './auth.service';
+import { AuthServices } from "./auth.service";
+import AppError from "../../errorHelpers/AppError";
 
-const credentialLogin = catchAsync(
-  async (req: Request, res: Response) => {
-    const loginInfo = await AuthServices.credentialLogin(req.body);
+const credentialLogin = catchAsync(async (req: Request, res: Response) => {
+  const loginInfo = await AuthServices.credentialLogin(req.body);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      message: "User Logged In Successfully",
-      data: loginInfo,
-      success: true,
-    });
+  res.cookie("accessToken", loginInfo.accessToken, {
+    httpOnly: true,
+    secure: false,
+  })
+
+  res.cookie("refreshToken", loginInfo.refreshToken, {
+    httpOnly: true,
+    secure: false,
+  })
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: "User Logged In Successfully",
+    data: loginInfo,
+    success: true,
+  });
+});
+
+const getNewAccessToken = catchAsync(async (req: Request, res: Response) => {
+  const { refreshToken } = req.cookies;
+
+  if(!refreshToken) {
+    throw new AppError(httpStatus.BAD_REQUEST, "No refresh token found");
   }
-);
 
-const getNewAccessToken = catchAsync(
-  async (req: Request, res: Response) => {
-    const refreshToken = req.cookies.refreshToken;
-    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
+  const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      message: "User Logged In Successfully",
-      data: tokenInfo,
-      success: true,
-    });
-  }
-);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: "Token Refreshed Successfully",
+    data: tokenInfo,
+    success: true,
+  });
+});
 
 export const AuthControllers = {
-    credentialLogin,
-    getNewAccessToken
-}
+  credentialLogin,
+  getNewAccessToken,
+};
